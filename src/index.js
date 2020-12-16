a0f0435774366042122cd93cbbfe1c9268c19272
Vlad <vladislavserov000@gmail.com>
2020-12-16 20:31:45 +0300

    remove unnesassery staff

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import store from './store/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
-window.store = store;
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>

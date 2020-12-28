6d8cf70085b0d50f50bc5ba350c45f18b3efb644
Vlad <vladislavserov000@gmail.com>
2020-12-28 16:12:30 +0300

    half fun with api

--- src/components/FilmSearch/index.js
+++ src/components/FilmSearch/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import { Typeahead } from 'react-typeahead';
+import { useState } from 'react';
+
+// http://www.omdbapi.com/?s=star&apikey=7019ebd8
 
 export default () => {
-    // const [selections, setSelections] = useState([]);
+    const [options, setOptions] = useState([]);
+
+    const handleSearchFieldChange = (event) => {
+        const searchValue = event.target.value;
+        const apiKey = '7019ebd8';
+        const requestStr = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apiKey}`;
+        axios.get(requestStr).then((response) => {
+            if (response.data.Response === 'True') {
+                const options = response.data.Search.map(({Title}) => Title);
+                setOptions(options);
+            }
+            console.log(response);
+        }, () => {})
+    }
 
     return (
-        <Typeahead
-            options={['John', 'Paul', 'George', 'Ringo']}
+        <Typeahead onKeyUp={handleSearchFieldChange}
+            options={options}
             maxVisible={2}
         />
     );
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import FilmSearch from '../../components/FilmSearch';
+
+// const promise = axios.get('https://google.com?q=123')
+
+// promise.then((response) => {}, (err) => {})
 
 const Main = (props) => {
     const { films } = props;
-
     return (
         <>
+            <div className="header">
+                <FilmSearch />
+            </div>
             { films.map((film) =>
                 <>
                     <img src={film.Poster} alt={film.Title}/>

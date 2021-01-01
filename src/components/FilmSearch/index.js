b868afa9683a73115921c20346d5944763def8b2
Vlad <vladislavserov000@gmail.com>
2021-01-01 12:25:45 +0300

    styling typeahead

--- src/components/FilmSearch/index.js
+++ src/components/FilmSearch/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import axios from 'axios';
 import { Typeahead } from 'react-typeahead';
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // http://www.omdbapi.com/?s=star&apikey=7019ebd8
 
 export default () => {
+    const history = useHistory();
     const [options, setOptions] = useState([]);
 
     const handleSearchFieldChange = (event) => {
@@ -14,17 +16,26 @@ export default () => {
         const requestStr = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apiKey}`;
         axios.get(requestStr).then((response) => {
             if (response.data.Response === 'True') {
-                const options = response.data.Search.map(({Title}) => Title);
+                const options = response.data.Search.map(({Title , imdbID}) => ({ title: Title , imdbId: imdbID }));
                 setOptions(options);
             }
             console.log(response);
         }, () => {})
     }
 
+    const handleOptionSelected = ({ imdbId }) => {
+        // console.log(event);
+        // redirect
+        history.push(`/film/${ imdbId }`);
+    }
+
     return (
         <Typeahead onKeyUp={handleSearchFieldChange}
+            onOptionSelected={handleOptionSelected}
+            filterOption={'title'}
+            displayOption={'title'} 
             options={options}
-            maxVisible={2}
+            maxVisible={6}
         />
     );
 };

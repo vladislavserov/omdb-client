56f404c274820ebf922adb6ccae2cf7d1299f1c5
vlad <vladislavserov000@gmail.com>
2021-04-15 12:49:16 +0300

    addGitpages

---
+++ docs/precache-manifest.e8dc8eaef8c041306ea0bf96e5715c43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cef2f8f34cb96c59228ac56122377a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "061843156f7dc4725a87",
+    "url": "/static/css/2.84d6edd9.chunk.css"
+  },
+  {
+    "revision": "08584b1d7efc4361a6f7",
+    "url": "/static/css/main.8d0195dc.chunk.css"
+  },
+  {
+    "revision": "061843156f7dc4725a87",
+    "url": "/static/js/2.587b93d0.chunk.js"
+  },
+  {
+    "revision": "1e0a7857e4cdeeae14b4674d19c1d353",
+    "url": "/static/js/2.587b93d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08584b1d7efc4361a6f7",
+    "url": "/static/js/main.9adeb130.chunk.js"
+  },
+  {
+    "revision": "23cda38e421279ad703a",
+    "url": "/static/js/runtime-main.951f087b.js"
+  }
+]);
